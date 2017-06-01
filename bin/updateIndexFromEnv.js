160216911b2ddfb85058acc7b505ec1cb2110754
udara <udara.weera@gmail.com>
2017-06-01 11:50:00 -0400

    remove strict eslint rule

--- bin/updateIndexFromEnv.js
+++ bin/updateIndexFromEnv.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 /* global require */
-"use strict";
+'use strict';
 const fs = require('fs');
 const path = require('path');
 const log = require('mozlog')('server');
